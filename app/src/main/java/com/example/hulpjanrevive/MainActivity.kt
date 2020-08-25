f9d931b5ff7c664c08f3e30bfd81b333aed41f0e
dennis <dennisgijzen@gmail.com>
2020-08-25 16:46:55 +0200

    inital appbar and navigation drawer set up with navController

--- app/src/main/java/com/example/hulpjanrevive/MainActivity.kt
+++ app/src/main/java/com/example/hulpjanrevive/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.hulpjanrevive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.main_drawer_layout)
+        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar))
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer)
+        val appBarConfiguration = AppBarConfiguration(setOf(), drawerLayout) //TODO add menu items for each destination
+        val navController = findNavController(R.id.host_fragment)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        findViewById<NavigationView>(R.id.navigation_view).setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.navigation_view, menu)
+        return true
     }
 }
