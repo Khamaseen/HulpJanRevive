21a40058b75b92c9fa334786baf3c6a57aa26933
dennis <dennisgijzen@gmail.com>
2020-08-28 08:16:30 +0200

    last update for switch

--- app/src/main/java/com/example/hulpjanrevive/MainViewModel.kt
+++ app/src/main/java/com/example/hulpjanrevive/MainViewModel.kt
@@ -5,6 +5,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 
 class MainViewModel(
@@ -21,6 +24,7 @@ class MainViewModel(
 
     init {
         getList()
+
     }
 
     fun addNumber() {
@@ -31,6 +35,13 @@ class MainViewModel(
     private fun getList() {
         viewModelScope.launch(dispatcher) {
             tasks.postValue(repository.returnMockTasks())
+        }
+    }
+
+    fun simple(): Flow<Int> = flow { // flow builder
+        for (i in 1..3) {
+            delay(100) // pretend we are doing something useful here
+            emit(i) // emit next value
         }
     }
 
